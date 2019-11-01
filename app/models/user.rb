eda59078b118e0c17b43852e7546f4db7d0b1702
iyuta1124 <iyuta1124@gmail.com>
2019-11-01 16:53:40 +0900

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,14 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-   # 渡されたトークンがダイジェストと一致したらtrueを返す
-   def authenticated?(remember_token)
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
-end 
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+end
