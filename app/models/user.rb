31518ab27cd22b58688a7dfbaa9be0e301a25c0f
iyuta1124 <iyuta1124@gmail.com>
2019-11-02 14:22:26 +0900

    Add account activation syusei

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,12 +41,6 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  # アカウントを有効にする
-  def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
-  end
-
   # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
@@ -56,7 +50,6 @@ class User < ApplicationRecord
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
   end
-
 
   private
 
